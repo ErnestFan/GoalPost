3b6973f7663c29eed8d0cf76027e32aa6d21a8b3
ErnestFan <ernestfan1019@gmail.com>
2018-04-13 21:58:20 -0700

    Created prototype tableview cell

---
+++ goalpost/View/GoalCell.swift
@@ -0,0 +1,22 @@
+//
+//  GoalCell.swift
+//  goalpost
+//
+//  Created by Ernest Fan on 2018-04-13.
+//  Copyright © 2018 ERF. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    @IBOutlet weak var goalDescriptionLbl: UILabel!
+    @IBOutlet weak var goalTypeLbl: UILabel!
+    @IBOutlet weak var goalProgressLbl: UILabel!
+    
+    func configureCell(description: String, type: String, goalProgressAmount: Int) {
+        self.goalDescriptionLbl.text = description
+        self.goalTypeLbl.text = type
+        self.goalProgressLbl.text = String(describing: goalProgressAmount)
+    }
+}
