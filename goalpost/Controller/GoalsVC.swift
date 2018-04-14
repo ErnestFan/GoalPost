51223b951241adb98b84867752d3f1170c682353
ErnestFan <ernestfan1019@gmail.com>
2018-04-13 21:32:27 -0700

    Created basic interface

---
+++ goalpost/Controller/GoalsVC.swift
@@ -0,0 +1,23 @@
+//
+//  GoalsVC.swift
+//  goalpost
+//
+//  Created by Ernest Fan on 2018-04-13.
+//  Copyright © 2018 ERF. All rights reserved.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func addGoalBtnWasPressed(_ sender: Any) {
+    }
+}
+
